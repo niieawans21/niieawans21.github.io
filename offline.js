1f113ce9b858f857b5526951cfb15fc3e2c438c8
niieawans21 <niieawans21@gmail.com>
2021-11-24 12:53:49 +0700

    Space X v1.1 Release 1.0

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1637732474,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background.png",
+		"images/player-sheet0.png",
+		"images/laser-sheet0.png",
+		"images/enemies-sheet0.png",
+		"images/enemiesparticle.png",
+		"images/buttonfire-sheet0.png",
+		"images/buttonup-sheet0.png",
+		"images/buttondown-sheet0.png",
+		"images/buttonright-sheet0.png",
+		"images/buttonleft-sheet0.png",
+		"media/sfx_laser1.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
